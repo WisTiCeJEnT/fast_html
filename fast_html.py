2388c7ea71823af767d82d0f9abfe6ab6437c8af
wattanai sathuphan <wattanai.sat@ku.th>
2020-02-05 22:10:57 +0700

    css upload page

--- fast_html.py
+++ fast_html.py
@@ -3,11 +3,54 @@ import os
 print(f"URL = {os.environ['URL']}/")
 
 UPLOAD_PAGE = f"""
-<html>
-    <form action="{os.environ['URL']}/" method="post">
-        <input type="text" name="code">
-        <input type="submit" value="Deploy ~">
-    </form>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <link
+      rel="stylesheet"
+      href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+      integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+      crossorigin="anonymous"
+    />
+
+    <title>Fast HTML Uploader</title>
+  </head>
+  <body>
+    <br />
+    <div class="container h-100 d-flex justify-content-center">
+      <form action="{os.environ['URL']}/" method="post">
+        <div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <h5 class="card-title">Fast HTML Uploader</h5>
+            <textarea name="code" name="description"></textarea>
+          </div>
+        </div>
+        <br />
+        <br />
+        <input type="submit" class="btn btn-dark" value="Deploy ~" />
+      </form>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script
+      src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+      integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+      integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+      integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+      crossorigin="anonymous"
+    ></script>
+  </body>
 </html>
 """
 
