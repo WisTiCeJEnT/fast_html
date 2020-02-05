cc70e9e99aefb92c76c31e3bc215b4cc407ce7cf
wattanai sathuphan <wattanai.sat@ku.th>
2020-02-05 21:28:31 +0700

    add page footer

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-import os
+from fast_html import *
 
 app = Flask(__name__)
 
@@ -12,30 +12,13 @@ def root():
     global code
     if request.method == 'POST':
         code = request.form['code']
-    return code # + UPLOAD_LINK     #Hide Upload link
+    return code + FOOTER # + UPLOAD_LINK     #Hide Upload link
 
 @app.route('/upload', methods=['GET'])
 def upload():
     return UPLOAD_PAGE
 
-UPLOAD_PAGE = f"""
-<html>
-    <form action="{os.environ['URL']}/" method="post">
-        <input type="text" name="code">
-        <input type="submit" value="Deploy ~">
-    </form>
-</html>
-"""
-
-UPLOAD_LINK = f"""
-<hr>
-<h1>Upload new code at
-<a href="{os.environ['URL']}/upload">{os.environ['URL']}/upload</a>
-</h1>
-"""
 code = ''
-
-print(f"URL = {os.environ['URL']}/")
 
 if __name__ == "__main__":
     app.run(debug = False,host="0.0.0.0", port=5000)
