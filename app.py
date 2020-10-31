f53d7642268b1cbf08ff2d572a68ca5c2a348553
finality5 <tanagon601@gmail.com>
2020-10-31 23:19:24 +0700

    code

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 
 @app.route('/test')
 def test():
+    print("Working")
     return "Working"
 
 @app.route('/', methods=['GET', 'POST'])
@@ -13,14 +14,17 @@ def root():
     global code
     if request.method == 'POST':
         code = request.form['code']
+        print(code)
     return code + FOOTER # + UPLOAD_LINK     #Hide Upload link
 
 @app.route('/upload', methods=['GET'])
 def upload():
+    print(code)
     return UPLOAD_PAGE
 
 @app.route('/code', methods=['GET'])
 def code():
+    print(code)
     return jsonify({"code": code})
 
 code = ''
